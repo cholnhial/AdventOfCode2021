0eaf45597c5615e8a98f527aae807bf66ef848f0
Chol Nhial <chol.nhial.chol@gmail.com>
2021-12-07 13:20:17 +1000

    solved Day 1 part two

--- src/com/cholnhial/day1/Day.java
+++ src/com/cholnhial/day1/Day.java
@@ -6,15 +6,18 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 public class Day {
 
     public static void main(String... args) throws Exception {
         solvePartOne();;
+        solvePartTwo();
     }
 
     private static void solvePartOne() throws Exception {
@@ -24,7 +27,40 @@ public class Day {
         List<Integer> numbers = lines.stream().map(Integer::valueOf).collect(Collectors.toList());
         int totalIncreases = getTotalIncreases(numbers);
 
-        System.out.printf("Total Increases: %d\n", totalIncreases);
+        System.out.printf("Solution 1: Total Increases: %d\n", totalIncreases);
+    }
+
+    private static void solvePartTwo() throws Exception {
+        FileUtil fileUtil = new FileUtil();
+        var lines = fileUtil.readFileFromResource("day1.input.txt");
+
+        List<Integer> numbers = lines.stream().map(Integer::valueOf).collect(Collectors.toList());
+        var slidingWindowGroups = getSlidingWindows(numbers);
+
+        List<Integer> sums = slidingWindowGroups
+                .stream().map(l -> l.stream().mapToInt(Integer::intValue)
+                .sum()).collect(Collectors.toList());
+
+        int totalIncreases = getTotalIncreases(sums);
+        System.out.printf("Solution 2: Total Increases: %d\n", totalIncreases);
+    }
+
+    public static List<List<Integer>> getSlidingWindows(List<Integer> numbers) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        int i =  0;
+        while(i < numbers.size()) {
+            List<Integer> threeNumbers = new ArrayList<>();
+            for (int j = 0; j < 3; j++) {
+                if (i+j < numbers.size()) {
+                    threeNumbers.add(numbers.get(i+j));
+                }
+            }
+            i++;
+            results.add(threeNumbers);
+        }
+
+        return results;
     }
 
     private static int getTotalIncreases(List<Integer> numbers) {
